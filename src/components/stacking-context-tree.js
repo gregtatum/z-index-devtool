a69afdce637f2895a2e43fe838bac5251031589b
James Reinlein <jamesreinlein@gmail.com>
2016-10-15 11:35:47 -0400

    Pull in tree from devtools
    I was unable to display the Tree inside the StackingContextTree.
    The nodes/tree is undefined until an action is called, and so when it goes
    to render the Tree, it complains about the props being undefined. We will
    have to delay the rendering of the Tree until that action is executed.

--- src/components/stacking-context-tree.js
+++ src/components/stacking-context-tree.js
@@ -1,6 +1,7 @@
 const {DOM, createClass, createFactory} = require("react");
 const {div} = DOM;
 const StackingContextNode = createFactory(require("./stacking-context-node"));
+const Tree = createFactory(require("./tree"));
 const {flattenTreeWithDepth} = require("./../stacking-context/")
 
 const StackingContextTree = createClass({
@@ -10,6 +11,18 @@ const StackingContextTree = createClass({
 
     return div(
         {id: "tree"},
+        // "nodes" is undefined until add-stacking-context action is executed
+        // so app complains about being unable to display a Tree due to the undefined props given
+        // Tree({
+        //     root: tree[0],
+        //     getRoots: () => [this.props.node],
+        //     getParent: item => item.parent,
+        //     getChildren: item => item.nodes,
+        //     getKey: () => {},
+        //     renderItem: () => {},
+        //     isExpanded: () => {},
+        //     itemHeight: 20
+        // }),
         ...nodes.map((node) => StackingContextNode({node}))
         );
   }
