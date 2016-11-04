be314805870c82f9780e441010bb1e49918e23d9
bria <bria@sfu.ca>
2016-11-03 17:11:14 -0700

    add resize handler to display-rectangle

--- src/components/display-rectangle.js
+++ src/components/display-rectangle.js
@@ -3,14 +3,46 @@ const {div, canvas} = DOM;
 const React = require("react");
 
 const DisplayRectangle = createClass({
+
+    // TODO - The bounding rect needs to be updated if there are DOM mutation events
+    // on the page that is being targetted. See
+    // https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver for more
+    // information on correctly handling this.
+    computeBoundingRect(elt) {
+      this.setState({
+        boundingRect: (elt) ? elt.getBoundingClientRect() : undefined
+      })
+    },
+
+    getInitialState() {
+      return {
+        handleResize: this.handleResize.bind(this)
+      }
+    },
+
+    handleResize() {
+      this.computeBoundingRect(this.props.elt);
+    },
+
+    componentWillReceiveProps(props) {
+      this.computeBoundingRect(props.elt);
+    },
+
+    componentWillMount() {
+      this.computeBoundingRect(this.props.elt);
+      window.addEventListener('resize', this.state.handleResize);
+    },
+
+    componentWillUnmount() {
+      window.removeEventListener('resize', this.state.handleResize);
+    },
+
     render: function() {
-      const {elt} = this.props;
-      const boundingRect = (elt) ? elt.getBoundingClientRect() : undefined;
+      const boundingRect = this.state.boundingRect;
       const hasNodeClass = (boundingRect) ? 'has-node' : 'no-node';
       return div(
         {
             className: "display-rectangle "+hasNodeClass,
-            //ref:'canvas',//call save el
             style:{
               width:(boundingRect)? boundingRect.width +'px' : 0,
               height:(boundingRect)? boundingRect.height +'px' : 0,
