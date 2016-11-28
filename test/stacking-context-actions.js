a57575e445e35fc338ceb0f1d07045ec4b48cf63
bria <bria@sfu.ca>
2016-11-27 17:21:25 -0800

    fix async issues with tests

--- test/stacking-context-actions.js
+++ test/stacking-context-actions.js
@@ -7,39 +7,43 @@ const {
 } = require('../src/actions/stacking-context');
 
 describe('store fixture', function() {
-    it('has url & text', function() {
+    it('has url & text', function(done) {
       createStoreFixture().then((store) =>{
-        assert.equal(store.getState().stackingContext.url, 'test/test.html');
-        assert.equal(store.getState().stackingContext.text, textMarkup);
+        assert.equal(store.getState().stackingContext.url, 'test/test.html', "URL");
+        assert.equal(store.getState().stackingContext.text, textMarkup, "Markup");
+        done();
+      }).catch((err) => {
+        done(err);
       });
     });
-    it('has tree', function() {
+    it('has tree', function(done) {
       createStoreFixture().then((store) =>{
-        assert.notEqual(store.getState().stackingContext.tree, undefined);
-        assert.equal(store.getState().stackingContext.tree.length, 2);
-      })
+        assert.notEqual(store.getState().stackingContext.tree, undefined, "Tree exists");
+        assert.equal(store.getState().stackingContext.tree.length, 2, "Size of Tree");
+        done();
+      }).catch((err) => {
+        done(err);
+      });
     });
 });
 
 describe('node expansion', function() {
-  it('expand first tree node', function() {
+  it('expand tree nodes', function(done) {
     createStoreFixture().then((store) =>{
       let expandedNodes = store.getState().stackingContext.expandedNodes;
+      assert.equal(expandedNodes.size, 0, "Size of expandedNodes 0");
       const tree = store.getState().stackingContext.tree;
       store.dispatch(expandNode(tree[0]));
-      assert.equal(expandedNodes.size, 1);
-      assert.equal(expandedNodes.get(0), tree[0]);
-    });
-  });
-
-  it('expand two tree nodes', function() {
-    createStoreFixture().then((store) =>{
-      let expandedNodes = store.getState().stackingContext.expandedNodes;
-      const tree = store.getState().stackingContext.tree;
-      store.dispatch(expandNode(tree[0]));
+      expandedNodes = store.getState().stackingContext.expandedNodes;
+      assert.equal(expandedNodes.size, 1, "Size of expandedNodes 1");
+      assert.equal(expandedNodes.has(tree[0]), true, "Tree Element 0");
       store.dispatch(expandNode(tree[1]));
-      assert.equal(expandedNodes.size, 2);
-      assert.equal(expandedNodes.get(1), tree[1]);
+      expandedNodes = store.getState().stackingContext.expandedNodes;
+      assert.equal(expandedNodes.size, 2, "Size of expandedNodes 2");
+      assert.equal(expandedNodes.has(tree[1]), true, "Tree Element 1");
+      done();
+    }).catch((err) => {
+      done(err);
     });
   });
 
@@ -47,28 +51,26 @@ describe('node expansion', function() {
 });
 
 describe('node collapse', function() {
-  it('collapse first tree node', function() {
+  it('collapse tree nodes', function(done) {
     createStoreFixture().then((store) =>{
       let expandedNodes = store.getState().stackingContext.expandedNodes;
+      assert.equal(expandedNodes.size, 0, "init- Size of expandedNodes 0");
       const tree = store.getState().stackingContext.tree;
       store.dispatch(expandNode(tree[0]));
       store.dispatch(expandNode(tree[1]));
+      expandedNodes = store.getState().stackingContext.expandedNodes;
+      assert.equal(expandedNodes.size, 2, "Size of expandedNodes 2");
       store.dispatch(collapseNode(tree[0]));
-      assert.equal(expandedNodes.size, 1);
-      assert.notEqual(expandedNodes.has(tree[0]), true);
-    });
-  });
-
-  it('collapse two tree nodes', function() {
-    createStoreFixture().then((store) =>{
-      let expandedNodes = store.getState().stackingContext.expandedNodes;
-      const tree = store.getState().stackingContext.tree;
-      store.dispatch(expandNode(tree[0]));
-      store.dispatch(expandNode(tree[1]));
-      store.dispatch(collapseNode(tree[0]));
+      expandedNodes = store.getState().stackingContext.expandedNodes;
+      assert.equal(expandedNodes.size, 1, "Size of expandedNodes 1");
+      assert.notEqual(expandedNodes.has(tree[0]), true, "Tree Element 0");
       store.dispatch(collapseNode(tree[1]));
-      assert.equal(expandedNodes.size, 0);
-      assert.notEqual(expandedNodes.has(tree[1]), true);
+      expandedNodes = store.getState().stackingContext.expandedNodes;
+      assert.equal(expandedNodes.size, 0, "Size of expandedNodes 0");
+      assert.notEqual(expandedNodes.has(tree[1]), true, "Tree Element 1");
+      done();
+    }).catch((err) => {
+      done(err);
     });
   });
 
