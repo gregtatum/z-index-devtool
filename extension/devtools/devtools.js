75560c39f5dd45ec882caf26bbb6a7ac1d41c29c
Hubert Boma Manilla <b4bomsy@gmail.com>
2017-12-23 17:52:39 +0000

    switch image

--- extension/devtools/devtools.js
+++ extension/devtools/devtools.js
@@ -6,7 +6,7 @@ function onHidden() {
   console.log("hidden");
 }
 browser.devtools.panels
-  .create("Z Index", "icons/stack.png", "devtools/panel/panel.html")
+  .create("Z Index", "icons/stack.svg", "devtools/panel/panel.html")
   .then(panel => {
     panel.onShown.addListener(onShown);
     panel.onHidden.addListener(onHidden);
